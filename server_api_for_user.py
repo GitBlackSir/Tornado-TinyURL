31f781968ac386b35825e880c3413428a9dea429
GitBlackSir <gitblacksir@gmail.com>
2018-05-01 20:40:25 +0800

    💻 两个API服务端 暂时移除MySQL管理模块

--- server_api_for_user.py
+++ server_api_for_user.py
@@ -5,9 +5,12 @@
 
 import json
 import getopt
+import sys
+
 import client as client
 import tornado.web
 import tornado.ioloop
+import src.orm as by_mysql
 import conf.conf as conf
 import tornado.httpserver
 import src.server_api_usage as server_api_usage
@@ -17,27 +20,27 @@ class Api_Long_To_Short_URL_Handler(tornado.web.RequestHandler):
     def get(self,*args):
         try:
             if args[0] == '':
-                dict = {'status':1,'messages':'EMPTY URL!', 'short_url': None}
+                dict = {'status':0,'messages':'EMPTY URL!', 'short_url': None}
                 self.write(json.dumps(dict))
-            elif args[0].split("/")[2] == "t.1024bit.io":
-                dict = {'status':2,'messages':' FORBID INPUT TINYURL!','short_url':None }
+            elif args[0].split("/")[2] == conf.server_api_for_user['SPILT_HOST_NAME']:
+                dict = {'status':1,'messages':'FORBID INPUT TINYURL!','short_url':None }
                 self.write(json.dumps(dict))
             else:
                 if is_true_url.is_url(args[0]):
                     if conf.redis_conn.get(args[0]):
-                        dict = {'status':3, 'messages':'this short_url from redis', 'short_url': ['http://t.1024bit.io/' + conf.redis_conn.get(args[0]).decode(), ]}
+                        dict = {'status':2, 'messages':'this short_url from redis', 'short_url': conf.server_api_for_user['HOST_NAME'] + conf.redis_conn.get(args[0]).decode(), }
                         self.write(json.dumps(dict))
                     else:
                         id_url_dict = client.get_guid()
                         conf.redis_conn.set(id_url_dict['short_url'], args[0])
                         conf.redis_conn.set(args[0], id_url_dict['short_url'])
-                        dict = {'status': 4, 'messages': 'server api for develop successful','short_url': ['http://t.1024bit.io/' + id_url_dict['short_url'], ]}
+                        dict = {'status': 3, 'messages': 'server api for develop successful','short_url': conf.server_api_for_user['HOST_NAME']+ id_url_dict['short_url']}
                         self.write(json.dumps(dict))
                 else:
-                    dict = {'status': 5, 'messages':'URL INPUT ERRO!', 'short_url':None}
+                    dict = {'status': 4, 'messages':'URL INPUT ERRO!', 'short_url':None}
                     self.write(json.dumps(dict))
         except:
-            data = {'status': 6, 'messages':'URL ERRO!','short_url':None }
+            data = {'status': 5, 'messages':'URL ERRO!','short_url':None }
             self.write(json.dumps(data))
 
 
@@ -53,7 +56,7 @@ class Api_Short_To_Long_URL_Handler(tornado.web.RequestHandler):
                         dict = {'status': 1, 'messages': 'successfully short to lang url', 'long_url': conf.redis_conn.get(args[0].split('/')[3]).decode()}
                         self.write(json.dumps(dict))
                     except:
-                        dict = {'status': 2, 'messages': 'url eg:http://t.1024bit.io/+xxxxxxxxx','long_url': None}
+                        dict = {'status': 2, 'messages': 'URL EG:'+ conf.server_api_for_user['HOST_NAME'] +'xxxxx','long_url': None}
                         self.write(json.dumps(dict))
                 else:
                     dict = {'status': 3, 'messages': 'URL ERRO!', 'long_url': None}
@@ -71,23 +74,23 @@ class Api_For_User_Application(tornado.web.Application):
 
 if __name__ == '__main__':
     try:
-        # options, args = getopt.getopt(sys.argv[1:], "lhmi:p:d:w:",
-        #                               ['list', 'help', 'init_mysql' "ip=", "port=", "datacenter=", "worker="])
-        # for name, value in options:
-        #     if name in ('-h', '--help'):
-        #         server_api_usage.usage()
-        #         sys.exit()
-        #     if name in ('-l', '--list'):
-        #         by_mysql.list_host_mysql()
-        #         sys.exit()
-        #     elif name in ('-i', '--ip'):
-        #         R_ADDRESS = value
-        #     elif name in ('-p', '--port'):
-        #         PORT = int(value)
-        #     elif name in ('-d', '--datacenter'):
-        #         DC_ID = int(value)
-        #     elif name in ('-w', '--worker'):
-        #         WORKER_ID = int(value)
+        options, args = getopt.getopt(sys.argv[1:], "lhmi:p:d:w:",
+                                      ['list', 'help', 'init_mysql' "ip=", "port=", "datacenter=", "worker="])
+        for name, value in options:
+            if name in ('-h', '--help'):
+                server_api_usage.usage()
+                sys.exit()
+            if name in ('-l', '--list'):
+                by_mysql.list_host_mysql()
+                sys.exit()
+            elif name in ('-i', '--ip'):
+                conf.server_api_for_user['ADDRESS'] = value
+            elif name in ('-p', '--port'):
+                conf.server_api_for_user['PORT']= int(value)
+            elif name in ('-d', '--datacenter'):
+                DC_ID = int(value)
+            elif name in ('-w', '--worker'):
+                WORKER_ID = int(value)
         http_server = tornado.httpserver.HTTPServer(Api_For_User_Application())
         print("Starting ID Server start at %s:%d" % (conf.server_api_for_user['ADDRESS'], conf.server_api_for_user['PORT']))
         http_server.listen(conf.server_api_for_user['PORT'],conf.server_api_for_user['ADDRESS'])
