7ee3f39dc6770b7ffff9839b6d8e896803d12681
GitBlackSir <gitblacksir@gmail.com>
2018-05-02 23:45:29 +0800

    🐥 API使用Demo示例 BootStrap4

--- web/tornado_web/controller/server_web_Handler.py
+++ web/tornado_web/controller/server_web_Handler.py
@@ -12,36 +12,53 @@ class HomeHandler(tornado.web.RequestHandler):
         self.render("index.html")
 
     def post(self):
+        try:
             if self.get_argument('dat',None):
                 dict = (client.Client('127.0.0.1','2222',str(self.get_argument('dat',None))).get_tinyurl())
                 print(dict)
                 if dict['short_url']:
-                    data = {'status': 1, 'messages': 'erro', 'data': [dict['short_url'], ]}
+                    data = {'status': 0, 'messages': 'erro', 'data': [dict['short_url'], ]}
                     self.write(json.dumps(data))
                 else:
-                    data = {'status': 0, 'messages': 'erro', 'data': [dict['messages'], ]}
+                    data = {'status': 1, 'messages': 'erro', 'data': [dict['messages'], ]}
                     self.write(json.dumps(data))
-class RestoreHandler(tornado.web.RequestHandler):
-    # def get(self):
-    #     self.render("restore.html")
+        except:
+            data = {'status': 2, 'messages': 'erro', 'data': ['API_USER ERROR!', ]}
+            self.write(json.dumps(data))
 
+class RestoreHandler(tornado.web.RequestHandler):
     def post(self):
         if self.get_argument('dat', None):
-            dict = (client.Client('127.0.0.1', '2222', str(self.get_argument('dat', None))).get_longurl())
-            if dict['long_url']:
-                data = {'status': 1, 'messages': 'erro', 'data': [dict['long_url'], ]}
-                self.write(json.dumps(data))
-            else:
-                data = {'status': 0, 'messages': 'erro', 'data': [dict['messages'], ]}
+            try:
+                dict = (client.Client('127.0.0.1', '2222', str(self.get_argument('dat', None))).get_longurl())
+                print(dict)
+                if dict['long_url']:
+                    data = {'status': 0, 'messages': 'erro', 'data': [dict['long_url'], ]}
+                    self.write(json.dumps(data))
+                else:
+                    data = {'status': 1, 'messages': 'erro', 'data': [dict['messages'], ]}
+                    self.write(json.dumps(data))
+            except:
+                data = {'status': 2, 'messages': 'erro', 'data': ['API_USER ERROR!', ]}
                 self.write(json.dumps(data))
 
-class AnonymousHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("anonymous.html")
+# class BaseHandler(tornado.web.RequestHandler):
+#     def get(self):
+#         self.write_error(404)
+#     def write_error(self, status_code, **kwargs):
+#         if status_code == 404:
+#             self.redirect("http://1024bit.io/blog/2018/05/01/基于分布式发号器的短网址系统API-Reference/")
+#         elif status_code == 500:
+#             self.render('public/500.html')
+#         else:
+#             self.write('error:' + str(status_code))
+# class AnonymousHandler(tornado.web.RequestHandler):
+#     def get(self):
+#         self.render("anonymous.html")
 
 class ApiHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("api.html")
+        self.redirect("http://1024bit.io/blog/2018/05/01/基于分布式发号器的短网址系统API-Reference/")
 
 class BlogHandler(tornado.web.RequestHandler):
     def get(self):
